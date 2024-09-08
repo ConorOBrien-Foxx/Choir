9cbf3629667f972466cf717e45b94b7ebecef490
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2024-09-07 20:06:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Choir
+
+`words_alpha.js` modified from https://github.com/dwyl/english-words .
+
+## Description
+
+The high bit of every 8-bit byte indicates whether the byte is a literal or an instruction. In practice, the character `/` is used in the literate version of the language to signal a run of high/low bits (initially high). For example, the following literate program corresponds to the following bytes:
+
+```
+literate program
+    abc/def
+bytes without /
+    01100001 01100010 01100011 01100100 01100101 01100110
+with high bits adjusted
+    11100001 11100010 11100011 01100100 01100101 01100110
+corresponding program
+    áâãdef
+```
+
+Therefore, `/` have no associated byte cost.
+
+## Instructions
+
+`N` refers to a number, `S` to a string, and `X` to anything. A tilde `~` indicates the argument order doesn't matter.
+
+```
+command     stack       effect
+⁺           [N]         [N]: increment; N + 1
+⁻           [N]         [N]: decrement; N - 1
+#           []          []: erases the current build string
+$           [X1,X2]     [X1]: drop top of stack
++           [N1,N2]     [N]: addition; N1 + N2
+-           [N1,N2]     [N]: subtract; N1 - N2
+:           [X]         [X,X]: duplicate top of stack
+@           [S,N]~      [S]: Nth character in S
+C           [S,N]~      [C]: center; middle N characters of S
+H           [S,N]~      [S]: head; first N characters of S
+I           [N]         [N]: add 2; double increment; N + 2
+J           [N]         [N]: subtract 2; double decrement; N - 2
+T           [S,N]~      [S]: tail; last N characters of S
+c           [S1,S2]     [N]: S1 concatenated with S2
+d           []          []: interpret next literal section as compressed
+}           []          [S]: pushes the currently built string up til now
+~           [X1,X2]     [X2,X1]: swap top two on stack
+```
+
+## 7-Bit code page
+
+```
+  0123456789ABCDEF
+0 ¬¡±×÷Œœµ«»¶ΣΠ§¡¿
+1 ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ⁿ
+2 □!"#$%&'()*+,-./
+3 0123456789:;<=>?
+4 @ABCDEFGHIJKLMNO
+5 PQRSTUVWXYZ[\]^_
+6 `abcdefghijklmno
+7 pqrstuvwxyz{|}~⌂
+```
+
+## Compression modes
+
+All compression modes index into a dictionary. Similar to Jelly's compression.
+
+All of these modes work by parsing out the bits corresponding to a base-255 integer, and then parsing that integer as indices into a list.
+
+### Mode 1 (`d`)
+
+Uses the hi bit system to demarcate sections. This "burns" 1 hi-bit every 7 bits. After 8 bytes used to encode, it is less efficient than `e`.
+
+### Mode 2 (`e`)
+
+Uses the next byte as the number of bytes to read next. Then, reads that many bytes. Overhead of 8 bits, but more efficient than `d` after 8 bytes.
+
+### Mode 3 (`h`...`⌂`)
+
+Uses the delimeter `⌂` to terminate strings.
---
+++ choir.js
@@ -0,0 +1,605 @@
+// 
+const CODE_PAGE = "¬¡±×÷Œœµ«»¶ΣΠ§¡¿⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ⁿ□!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂";
+
+const toBase = (n, base) => {
+    n = BigInt(n);
+    base = BigInt(base);
+    let digits = [];
+    while(n >= 1n) {
+        digits.unshift(Number(n % base));
+        n /= base;
+    }
+    return digits;
+};
+
+const fromBase = (arr, base) => {
+    base = BigInt(base);
+    let output = 0n;
+    for(let x of arr) {
+        output *= base;
+        output += BigInt(x);
+    }
+    return output;
+};
+
+const bytesToLiterateChoir = bytes => {
+    let isLiteral = true;
+    let result = "";
+    for(let byt of bytes) {
+        let currentIsLiteral = !!(byt & 0b10000000);
+        if(currentIsLiteral !== isLiteral) {
+            result += "/";
+            isLiteral = currentIsLiteral;
+        }
+        let lower = byt & 0b01111111;
+        result += CODE_PAGE[lower];
+    }
+    return result;
+};
+
+const choirLiterateToBytes = str => {
+    let isLiteral = true;
+    let bytes = [];
+    for(let [ ins ] of str.matchAll(/\/\/|./g)) {
+        if(ins === "//") {
+            ins = "/";
+        }
+        else if(ins === "/") {
+            isLiteral = !isLiteral;
+            continue;
+        }
+        
+        let ord = CODE_PAGE.indexOf(ins);
+        ord += isLiteral << 7;
+        bytes.push(ord);
+    }
+    console.log(bytes);
+    return bytes;
+};
+
+const swapCase = str => 
+    str.replace(/\w/g,
+        chr => chr == chr.toLowerCase()
+            ? chr.toUpperCase()
+            : chr.toLowerCase()
+    );
+
+const indexTrieFromWordList = wordList => {
+    let indexTrie = {};
+    wordList.forEach((word, idx) => {
+        let iter = indexTrie;
+        for(let ch of [...word, null]) {
+            if(ch === null) {
+                iter[ch] = idx;
+            }
+            else {
+                iter[ch] ||= {};
+                iter = iter[ch];
+            }
+        }
+    });
+    return indexTrie;
+};
+
+const longestPrefixInIndexTrie = (str, indexTrie) => {
+    let longestIndex = -1;
+    let trieIndex = -1;
+    let iter = indexTrie;
+    for(let i = 0; i < str.length; i++) {
+        iter = iter[str[i]];
+        // console.log(iter);
+        if(!iter) {
+            break;
+        }
+        if(null in iter) {
+            longestIndex = i;
+            trieIndex = iter[null];
+        }
+    }
+    return { longestIndex, trieIndex };
+};
+
+const SHORT_WORDS_TRIE = indexTrieFromWordList(SHORT_WORDS);
+const WORDS_TRIE = indexTrieFromWordList(WORDS);
+
+const compressStringToBigInt = str => {
+    // console.log("-------------- COMPRESSING --------------");
+    
+    // isolate into parts
+    let parts = [];
+    for(let i = 0; i < str.length; i++) {
+        // ignore spaces, for now TODO: don't
+        let spaceStart = str[i] === " ";
+        if(spaceStart) {
+            i++;
+        }
+        
+        let head = str.slice(i);
+        let headSwapped = swapCase(head);
+        // TODO: check for swapcase
+        let longestIndex = -1;
+        let dictionaryIndex = -1;
+        let wordIndex = -1;
+        let isSwap = false;
+        let word;
+        for(let headIter of [ head, headSwapped ]) {
+            for(let didx of [ 0, 1 ]) {
+                let trie = didx === 0 ? SHORT_WORDS_TRIE : WORDS_TRIE;
+                let {
+                    longestIndex: stringIndexCombo,
+                    trieIndex: trieIndexCombo,
+                } = longestPrefixInIndexTrie(headIter, trie);
+                
+                if(stringIndexCombo > longestIndex) {
+                    longestIndex = stringIndexCombo;
+                    dictionaryIndex = didx;
+                    wordIndex = trieIndexCombo;
+                    isSwap = headIter === headSwapped;
+                    word = headIter.slice(0, longestIndex + 1);
+                }
+            }
+        }
+        
+        if(longestIndex !== -1) {
+            parts.unshift({
+                word,
+                wordIndex,
+                dictionaryIndex,
+                spaceStart,
+                isFirst: parts.length === 0,
+                isSwap,
+            });
+            i += longestIndex;
+        }
+        else {
+            if(spaceStart) {
+                i--;
+            }
+            parts.unshift({ word: str[i] });
+        }
+        
+    }
+    
+    // compress parts back-to-front
+    // unwrapping is a reverse process, so we start at the end
+    // we stored the parts in the reverse order already
+    let n = 0n;
+    console.log("parts:", JSON.stringify(parts));
+    for(let part of parts) {
+        let { word } = part;
+        let interpretMode = 0n;
+        if(word.length === 1) {
+            if(word < ' ' || word > '~') {
+                console.warn("Cannot encode character", word);
+                word = ' ';
+            }
+            let ord = word.charCodeAt(0);
+            n *= 96n;
+            n += BigInt(ord - 32) + 1n;
+            console.log("after char", n);
+        }
+        else {
+            let {
+                wordIndex,
+                dictionaryIndex,
+                spaceStart,
+                isFirst,
+                isSwap,
+            } = part;
+            let dict = dictionaryIndex === 0 ? SHORT_WORDS : WORDS;
+            interpretMode = 1n;
+            
+            n *= BigInt(dict.length);
+            n += BigInt(wordIndex);
+            
+            n *= 2n;
+            n += BigInt(dictionaryIndex);
+            
+            let flag = 0n;
+            if(isFirst && spaceStart) {
+                interpretMode = 2n;
+                flag = 1n;
+            }
+            if(!isFirst && !spaceStart) {
+                interpretMode = 2n;
+                flag = 1n;
+            }
+            if(isSwap) {
+                interpretMode = 2n;
+                if(flag === 1n) {
+                    flag = 2n;
+                }
+            }
+            
+            if(interpretMode === 2n) {
+                // TODO: flag
+                n *= 3n;
+                n += flag;
+            }
+        }
+        n *= 3n;
+        n += interpretMode;
+    }
+    
+    return n;
+    
+    // return n;
+};
+
+const decompressStringFromBigInt = n => {
+    // console.log("-------------- DECOMPRESSING --------------");
+    let result = "";
+    n = BigInt(n);
+    while(n) {
+        let interpretMode = n % 3n;
+        // console.log({ interpretMode });
+        n /= 3n;
+        if(interpretMode === 0n) {
+            // interpret as ASCII character
+            // console.log("ASCII character interpret", n);
+            let asciiOrd = n % 96n;
+            n /= 96n;
+            let chr = String.fromCharCode(Number(asciiOrd) + 32 - 1);
+            result += chr;
+        }
+        else {
+            // console.log("word index");
+            let flagSwapCase = false;
+            let flagPrependSpace = result != "";
+            if(interpretMode === 2n) {
+                // add space/casing information
+                let flag = n % 3n;
+                // there's technically 4 options (since there's 2 flags), but the 4th option is implicitly the case (interpretMode == 1)
+                n /= 3n;
+                flagSwapCase = flag != 1;
+                flagPrependSpace ^= flag != 0;
+                // console.log("Derivated flags:", {flag, flagSwapCase, flagPrependSpace});
+            }
+            // which dictionary do we wish to index into?
+            let dictionaryIndex = n % 2n;
+            n /= 2n;
+            // console.log("dictionary index", dictionaryIndex);
+            let dict = dictionaryIndex === 0n ? SHORT_WORDS : WORDS;
+            let wordIndex = n % BigInt(dict.length);
+            n /= BigInt(dict.length);
+            // TODO: flags
+            let word = dict[Number(wordIndex)];
+            // console.log({ word, wordIndex });
+            
+            if(flagSwapCase) {
+                word = swapCase(word);
+            }
+            
+            if(flagPrependSpace) {
+                word = " " + word;
+            }
+            
+            result += word;
+        }
+    }
+    return result;
+};
+
+const compressString = str => {
+    let n = compressStringToBigInt(str);
+    let bits = toBase(n, 2).map(n => Number(n));
+    
+    let mode1 = [ 0b10000000 + "d".charCodeAt() ];
+    let mode1bits = [...bits];
+    while(mode1bits.length % 7 !== 0) {
+        mode1bits.unshift(0);
+    }
+    for(let i = 0; i < mode1bits.length; i += 7) {
+        mode1.push(Number(fromBase(mode1bits.slice(i, i + 7), 2)));
+    }
+    return bytesToLiterateChoir(mode1);
+};
+// console.log(compressString("hello"));
+
+const extractChoirSections = commands => {
+    let sections = [];
+    let section = {
+        isLiteral: true,
+        commands: "",
+    };
+    
+    let isLiteral = true;
+    for(let cmd of commands) {
+        let highBit   = cmd & 0b10000000;
+        let lowerBits = cmd & 0b01111111;
+        if(!!highBit === isLiteral) {
+            isLiteral = !isLiteral;
+            sections.push(section);
+            section = {
+                isLiteral,
+                commands: "",
+            };
+        }
+        // console.log(cmd, highBit, isLiteral);
+        section.commands += CODE_PAGE[lowerBits];
+    }
+    
+    if(section.commands.length > 0) {
+        sections.push(section);
+    }
+    
+    return sections;
+};
+
+// TODO: BigInts
+const CHOIR_NORETURN = Symbol("CHOIR_NORETURN");
+const CHOIR_COMMANDS = {
+    // ¬
+    // ¡
+    // ±
+    // ×
+    // ÷
+    // Œ
+    // œ
+    // µ
+    // «
+    // »
+    // ¶
+    // Σ
+    // Π
+    // §
+    // ¡
+    // ¿
+    // ⁰
+    // ¹
+    // ²
+    // ³
+    // ⁴
+    // ⁵
+    // ⁶
+    // ⁷
+    // ⁸
+    // ⁹
+    "⁺": function increment(n) { return +n + 1; },
+    "⁻": function decrement(n) { return +n - 1; },
+    // ⁼
+    // ⁽
+    // ⁾
+    // ⁿ
+    // □
+    // !
+    // "
+    "#": function eraseBuildString() {
+        this.buildString = "";
+        return CHOIR_NORETURN;
+    },
+    "$": function dropTop(n) { return CHOIR_NORETURN; },
+    // %
+    // &
+    // '
+    // (
+    // )
+    // *
+    "+": function add(n1, n2) { return +n1 + +n2; },
+    // ,
+    "-": function subtract(n1, n2) { return +n1 - +n2; },
+    // .
+    // /
+    // 0
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
+    // 6
+    // 7
+    // 8
+    // 9
+    ":": function duplicate() {
+        return this.peek();
+    },
+    // ;
+    // <
+    // =
+    // >
+    // ?
+    "@": function elementAt(s, n) {
+        if(typeof s === "number" && typeof n === "string") {
+            [ s, n ] = [ n, s ];
+        }
+        
+        return s[n];
+    },
+    // A
+    // B
+    "C": function center(s, n) {
+        if(typeof s === "number" && typeof n === "string") {
+            [ s, n ] = [ n, s ];
+        }
+        
+        let start = s.length / 2;
+        let end = Math.ceil(start);
+        if(start === end) end++;
+        n--;
+        // console.log(start, end, n);
+        return s.slice(start - n, end + n);
+    },
+    // D
+    // E
+    // F
+    // G
+    "H": function head(s, n) {
+        if(typeof s === "number" && typeof n === "string") {
+            [ s, n ] = [ n, s ];
+        }
+        
+        return s.slice(0, n);
+    },
+    "I": function doubleIncrement(n) { return +n + 2; },
+    "J": function doubleDecrement(n) { return +n - 2; },
+    // K
+    // L
+    // M
+    // N
+    // O
+    // P
+    // Q
+    // R
+    // S
+    "T": function head(s, n) { 
+        if(typeof s === "number" && typeof n === "string") {
+            [ s, n ] = [ n, s ];
+        }
+        
+        return s.slice(-n);
+    },
+    // U
+    // V
+    // W
+    // X
+    // Y
+    // Z
+    // [
+    // \
+    // ]
+    // ^
+    // _
+    // `
+    // a
+    // b
+    "c": function concatenate(s1, s2) {
+        return s1 + s2;
+    },
+    "d": function setCompressMode1() {
+        this.compressMode = 1;
+        return CHOIR_NORETURN;
+    },
+    // e
+    // f
+    // g
+    // h
+    // i
+    // j
+    // k
+    // l
+    // m
+    // n
+    // o
+    // p
+    // q
+    // r
+    // s
+    "t": function titleCase(s) {
+        console.log("title case:", s);
+        return s.split(" ").map(word => word ? word[0].toUpperCase() + word.slice(1).toLowerCase() : word).join(" ");
+    },
+    // u
+    // v
+    // w
+    // x
+    // y
+    // z
+    // {
+    // |
+    "}": function pushBuild() {
+        let save = this.buildString;
+        this.buildString = "";
+        return save;
+    },
+    // ~
+    // ⌂
+};
+
+const evalChoir = (commands, input) => {
+    // split into sections: literal and instruction
+    
+    let state = {
+        buildString: "",
+        compressMode: 0,
+        stack: [],
+        popBuildString() {
+            let save = this.buildString;
+            this.buildString = "";
+            return save;
+        },
+        pop() {
+            // TODO: multiple inputs; reset inputs index on new sections
+            return this.stack.length
+                ? this.stack.pop()
+                : input
+                    ? input
+                    : this.popBuildString();
+        },
+        peek() {
+            // TODO: multiple inputs; reset inputs index on new sections
+            return this.stack.length ? this.stack.at(-1) : input || this.buildString;
+        },
+        resetForNewSection() {
+            this.stack = [];
+        },
+        endInstructionSection() {
+            let value = this.stack.length ? this.pop() : CHOIR_NORETURN;
+            if(value !== CHOIR_NORETURN) {
+                this.buildString += value;
+            }
+        },
+        popArguments(n) {
+            let args = [];
+            while(args.length < n) {
+                args.unshift(this.pop());
+            }
+            return args;
+        },
+    };
+    let sections = extractChoirSections(commands);
+    
+    for(let { isLiteral, commands } of sections) {
+        if(isLiteral) {
+            console.log("Appending literal section", commands, "under", state.compressMode);
+            if(state.compressMode === 0) {
+                state.buildString += commands;
+            }
+            else if(state.compressMode === 1) {
+                let totalBits = [...commands].flatMap(chr => {
+                    let bits = toBase(CODE_PAGE.indexOf(chr), 2);
+                    while(bits.length < 7) {
+                        bits.unshift(0);
+                    }
+                    return bits;
+                });
+                let n = fromBase(totalBits, 2);
+                console.log(totalBits, n);
+                state.buildString += decompressStringFromBigInt(n);
+            }
+            else {
+                console.warn("Unhandled compress mode:", state.compressMode);
+            }
+            state.compressMode = 0; // reset
+        }
+        else {
+            state.resetForNewSection();
+            for(let [ command, digits ] of commands.matchAll(/([0-9]+)|./g)) {
+                console.log(command);
+                let fn = CHOIR_COMMANDS[command];
+                if(fn) {
+                    let args = state.popArguments(fn.length);
+                    let value = fn.apply(state, args);
+                    if(value !== CHOIR_NORETURN) {
+                        state.stack.push(value);
+                    }
+                }
+                else if(digits) {
+                    state.stack.push(+digits);
+                }
+                else {
+                    console.warn("Unimplemented command", command);
+                }
+            }
+            state.endInstructionSection();
+        }
+    }
+    
+    console.log(state.buildString);
+    console.log(state);
+    
+    return state;
+};
+
+// evalChoir(choirLiterateToBytes(`2H/.../2T`), "AdmBorkBork");
+// evalChoir(choirLiterateToBytes(`:/spooky/2+/me`), 6);
+// evalChoir(choirLiterateToBytes(`:D/².2/I/me`), 6);
